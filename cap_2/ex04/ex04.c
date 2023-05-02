b3a9903dd126bcb442f8f4d9bbafdd0a0d16abc2
gguilherme42 <anoobs@tutamail.com>
2023-05-02 09:06:45 -0300

    ex04 cap 2

--- cap_2/ex04/ex04.c
+++ cap_2/ex04/ex04.c
@@ -2,7 +2,17 @@
 
 
 int main(void){
-	float angle_in_radians;
+	float angle_in_radians, angle_in_degrees, angle_in_minutes, angle_in_seconds;
+	printf("Digite um ângulo em radianos: ");
+	scanf("%f", &angle_in_radians);
+	angle_in_degrees = angle_in_radians * 57.29578;
+	angle_in_minutes = angle_in_degrees * 60;
+	angle_in_seconds = angle_in_minutes * 60;
+
+	printf("Ângulo em graus: %.2f\n", angle_in_degrees);
+	printf("Ângulo em minutos: %.2fm\n", angle_in_minutes);
+	printf("Ângulo em segundos: %.2fs\n", angle_in_seconds);
+	
 
 	return 0;
 }
