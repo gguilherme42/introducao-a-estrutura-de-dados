e6a43fbdd894e497c619d9fd29d9a63465ef78f2
gguilherme42 <anoobs@tutamail.com>
2023-05-15 11:11:57 -0300

    removendo função desnecessário. ex10 cap 6

--- cap_6/ex10/ex10.c
+++ cap_6/ex10/ex10.c
@@ -11,19 +11,6 @@ void print_v(int n, float  *vet){
 }
 
 
-void sort(int n, float *vet){
-	float cup = 0;
-	for (int i = 0; i < (n - 1); i++){
-		for (int j = i; j < n; j++) {
-			if (vet[i] > vet[j]){
-				cup = vet[i];
-				vet[i] = vet[j];
-				vet[j] = cup;
-			}
-		
-		}
-	}
-}
 
 
 void print_h(int n, int *h){
@@ -53,9 +40,6 @@ int main(void){
 	float lab_test[10] = {0.110,0.200,0.030,0.560,0.323,0.345,0.234,0.960,0.123,0.123};
 	int hist[10] = {0,0,0,0,0,0,0,0,0,0}; 
 	int len = 10;
-
-	//print_v(len, lab_test);
-	sort(len, lab_test);
 	histograma(len, lab_test, hist);
 	print_v(len, lab_test);
 	print_h(len, hist);
