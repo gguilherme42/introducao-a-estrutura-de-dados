dcadb67a2e7c90963322a7a6b17207792219e34d
gguilherme42 <anoobs@tutamail.com>
2023-05-16 11:07:07 -0300

    refatoração, ex12 cap 6

--- cap_6/ex12/ex12.c
+++ cap_6/ex12/ex12.c
@@ -6,7 +6,7 @@
 int is_diagonal(int line, int column){return line == column;}
 
 
-int is_not_identity(double A[][N], int l, int c){
+int is_not_identity(const double A[][N], int l, int c){
 	if (is_diagonal(l,c)) return A[l][c] != 1;
 	return A[l][c] != 0;
 
@@ -14,7 +14,7 @@ int is_not_identity(double A[][N], int l, int c){
 
 
 
-int is_identity(double A[][N]){
+int is_identity(const double A[][N]){
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
 			if (is_not_identity(A,i,j)) return 0;
@@ -24,13 +24,18 @@ int is_identity(double A[][N]){
 }
 
 
+void print_identity_check(const double A[][N]){
+	printf("Matriz identidade: %s\n", (is_identity(A) ? "SIM": "NÃO"));
+}
+
+
 int main(void){
 	double matrix[N][N] = {
 		{1, 0, 0},
 		{0, 1, 0},
 		{0, 0, 1}
 	};
-
-	printf("Matriz identidade: %s\n", (is_identity(matrix) ? "SIM": "NÃO"));
+	
+	print_identity_check(matrix);
 	return 0;
 }
