f45467eb539c597d714ca1ba279aba5d27ef7fa1
gguilherme42 <anoobs@tutamail.com>
2023-05-16 09:58:37 -0300

    ex11 cap 6

--- cap_6/ex11/ex11.c
+++ cap_6/ex11/ex11.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#define N 2
+#define N 3
 
 
 int triangular_inferior(double A[][N]){
 	int result = 1;
+	
+	int top_values_are_not_zero(int line, int column){
+		return (column > line && A[line][column] != 0);
+	}
+
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
-			if (i == j && A[i][j] != 0) return 0;
+			if (top_values_are_not_zero(i,j)) return 0; // valores acima da diagonal diferentes de 0
 		}
 	}
 	return result;
@@ -14,9 +19,13 @@ int triangular_inferior(double A[][N]){
 }
 
 int main(void){
-	double matrix[N][N] = {{0,0},{1,0}};
+	double matrix[N][N] = {
+				{0, 0, 0},
+				{1, 0, 2},
+				{1, 4, 0}};
 
-	printf("Matriz quadrada inferior: %d\n", triangular_inferior(matrix));
+
+	printf("Matriz quadrada inferior: %s\n", (triangular_inferior(matrix)? "SIM": "NÃO"));
 	return 0;
 }
 
