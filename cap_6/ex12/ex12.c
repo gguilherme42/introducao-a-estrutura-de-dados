bcd3740c7960d04d5c9e1e5475644aa075f4933c
gguilherme42 <anoobs@tutamail.com>
2023-05-16 10:49:52 -0300

    função is_not_identity colocada fora do escopo, ex12 cap 6

--- cap_6/ex12/ex12.c
+++ cap_6/ex12/ex12.c
@@ -6,17 +6,24 @@
 int is_diagonal(int line, int column){return line == column;}
 
 
+int is_not_identity(double A[][N], int l, int c){
+	if (is_diagonal(l,c)) return A[l][c] != 1;
+	return A[l][c] != 0;
+
+}
+
+
 
 int identidade(double A[][N]){
-	int is_not_identity(int line, int column){
-		if (is_diagonal(line,column)) return A[line][column] != 1;		
-		return A[line][column] != 0;
-	}
+	//int is_not_identity(int line, int column){
+	//	if (is_diagonal(line,column)) return A[line][column] != 1;		
+//		return A[line][column] != 0;
+//	}
 
 
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
-			if (is_not_identity(i,j)) return 0;
+			if (is_not_identity(A,i,j)) return 0;
 		}
 	}
 	return 1;
@@ -25,7 +32,7 @@ int identidade(double A[][N]){
 
 int main(void){
 	double matrix[N][N] = {
-		{1, 0, 2},
+		{1, 0, 0},
 		{0, 1, 0},
 		{0, 0, 1}
 	};
