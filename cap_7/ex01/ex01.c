dbdc61ff08b5eace75766b0fe42a3e7fc2a5d6a5
gguilherme42 <anoobs@tutamail.com>
2023-05-20 16:17:25 -0300

    ex01 cap 7, finalizado sem liberação da memória

---
+++ cap_7/ex01/ex01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_v(int len, float *vet){
+	printf("[");
+	
+	int is_last_element(int m){return (m + 1) == len;}
+	
+	for (int i = 0; i < len; i++){
+		if (is_last_element(i)) printf("%.2f", vet[i]);
+		else printf("%.2f, ", vet[i]);
+	}
+	printf("]\n");
+}
+
+
+float* reverso(int len, float *vet){
+	float* result = (float *) malloc(len * sizeof(float));
+	int c = 0;
+	for (int i = (len - 1); i >= 0; i--){
+		result[c] = vet[i];
+		c ++;
+	}
+	return result;
+
+}
+
+int main(void){
+	float numbers[5] = {0,1,2,3,4};
+	int l = 5;
+	//float* new_n;
+
+	print_v(l, numbers);
+	print_v(l, reverso(l, numbers));
+	return 0;
+}
