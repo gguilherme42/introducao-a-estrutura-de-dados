444437c194d9769a153c9972a9e5799fae873926
gguilherme42 <anoobs@tutamail.com>
2023-05-21 16:26:12 -0300

    grupo de estudos, ex02 → operação de divisão com a função de multiplicação

--- not_related/calc.c
+++ not_related/calc.c
@@ -7,11 +7,11 @@ int is_result_positive(int a, int b){return (a < 0) ^ (b < 0) ? 0 : 1;}
 
 
 int multiply(int a, int b){
-	int result = 0;
 	if (a == 0 || b == 0) return 0;
 	if (b == 1) return a;
 	if (a == 1) return b;
 	
+	int result = 0;
 	int cup = abs(b);
 	int mug = abs(a);
 	for (int i = 0; i < cup; i++){
@@ -31,6 +31,21 @@ int sub(int a, int b){
 
 }
 
+
+int div(int a, int b){
+	if (a == 0 || b == 0 || b > a) return 0;
+	if (b == 1) return a;
+	if (b == -1) return -a;
+	
+	int result = 1;
+	while (multiply(result, b) != a ){
+		result++;
+	}
+
+	return result;
+
+}
+
 int main(void){
 	int n1, n2;
 	printf("Valor 1: ");
@@ -40,6 +55,7 @@ int main(void){
 
 	printf("%d x %d = %d\n", n1, n2, multiply(n1,n2));
 	printf("%d - %d = %d\n", n1, n2, sub(n1,n2));
+	printf("%d / %d = %d\n", n1, n2, div(n1, n2));
 
 	return 0;
 }
