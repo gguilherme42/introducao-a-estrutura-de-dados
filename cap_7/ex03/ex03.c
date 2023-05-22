d676e1e9977a8cd706512cff7da22e9122331346
gguilherme42 <anoobs@tutamail.com>
2023-05-22 08:57:03 -0300

    ex03 cap 7, (c)

--- cap_7/ex03/ex03.c
+++ cap_7/ex03/ex03.c
@@ -20,14 +20,19 @@ float** create_square_bottom_matrix(int len){
 
 
 
+float access_matrix(int i, int j, float** m){
+	if (i >= j) return m[i][j];
+	return 0;
+}
+
 
 void print_m(int len,float** m){
-	printf("[");
+	printf("[\n");
 	for (int i =0; i < len; i++){
-		printf("[");
+		printf("\t[");
 		for (int j=0; j < len; j++){
-			if ((j + 1) == len) printf("%.2f", m[i][j]);
-			else printf("%.2f, ", m[i][j]);
+			if ((j + 1) == len) printf("%.2f", access_matrix(i, j, m));
+			else printf("%.2f, ", access_matrix(i,j,m));
 		}
 		printf("]\n");
 	}
@@ -44,13 +49,15 @@ void assign_to_matrix(int i, int j, float value, float ** m){
 
 
 
+
+
 int main(void){
 	int l = 3;
 	float ** matrix_A;
 
 	matrix_A = create_square_bottom_matrix(l);
 	assign_to_matrix(1,0,22, matrix_A);
-	//print_m(l, matrix_A);
+	print_m(l, matrix_A);
 	//printf("%.2f\n", matrix_A[1][0]);
 	return 0;
 }
