1078320ed49cfeb477fb6b8bc792458875a392bc
gguilherme42 <anoobs@tutamail.com>
2023-06-04 19:15:00 -0300

    ex03 cap 8, parte 1

--- cap_9/ex03/ex03.c
+++ cap_9/ex03/ex03.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 typedef struct rectangle Rectangle;
 typedef struct circle Circle;
@@ -11,14 +13,24 @@ struct rectangle{
 };
 
 
-Rectangle* circumscript_rect(Circ* c, float h){
-	Rect result;
+Rectangle* circumscript_rect(Circle* c, float h){
+	Rectangle* result = (Rectangle *) malloc( sizeof(Rectangle));
+	result->h = h;
+	float d = c->r * 2;
+
+	result->b = sqrt((d * d) - (result->h * result->h));
 	return result;
 
 }
 
 
 int main(void){
-	
+	Rectangle* test;
+	Circle a;
+	a.r = 5;
+
+	test = circumscript_rect(&a, 8);	
+	printf("h: %f | b: %f\n", test->h, test->b);
+	free(test);
 	return 0;
 }
